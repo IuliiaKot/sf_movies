1dcb3d2e77b97bc305e7dddf2aa506d37eb16830
IuliiaKot <kotlenko.julia@gmail.com>
2015-07-20 12:14:30 -0400

    full map

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,21 @@
 class MoviesController < ApplicationController
 
   def index
-
-     Movie.get_movie() if Movie.count == 0
-     @movies = Movie.select("distinct title").order("title")
-     @m = Movie.all
-
+    if params["movie.title"].nil?
+      Movie.get_movie() if Movie.count == 0
+      @movies = Movie.select("distinct title").order("title")
+      @m = Movie.all
+    else
+      @movies = Movie.select("distinct title").order("title")
+      @m = Movie.select('*').where('title=?', params["movie.title"])
+    end
     #  render :json => @m
+
+  end
+
+
+  def show
+
+    render 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'movies#index'
+   root 'movies#index'
+  # get 'movies' => 'movies#index'
+   post '/' => 'movies#index'
+  #resources :movies
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
