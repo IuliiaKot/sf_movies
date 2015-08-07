cd712e389fb54bbd537fd8a684ceffd0a55d6e08
IuliiaKot <kotlenko.julia@gmail.com>
2015-08-07 01:40:35 -0400

    add pums webserver on production, comment load part

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
 
   def index
-    
+
     if params["movie.title"].nil?
-      Movie.get_movie() if Movie.count == 0
+      #Movie.get_movie() if Movie.count == 0
       @movies = Movie.select("distinct title").order("title")
       @m = Movie.all
     else
